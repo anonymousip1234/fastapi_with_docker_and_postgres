9ec25fac41b3036db54a5877dc07591b9c5f70d5
Swapnonil <swapnonilmukherjee62@gmail.com>
2024-05-29 22:55:53 +0530

    extra comments added in the database section

--- defined_databases/__init__.py
+++ defined_databases/__init__.py
@@ -6,18 +6,21 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+#local database for testing purposes
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 
+#Postgres Database for production purposes
 POSTGRES_USER = os.getenv('POSTGRES_USER')
 POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
 POSTGRES_HOST = os.getenv('POSTGRES_HOST')
 POSTGRES_PORT = os.getenv('POSTGRES_PORT')
 POSTGRES_DB = os.getenv('POSTGRES_DB')
 
-DATABASE_URL = f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+POSTGRES_DATABASE_URL = f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
 
-engine = create_engine(DATABASE_URL)
+#to use local database just change the variable POSTGRES_DATABASE_URL to SQLALCHEMY_DATABASE_URL
+engine = create_engine(POSTGRES_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
 
